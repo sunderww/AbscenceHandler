50375027f7bbf8e3c1a3b8f7783f124a42a163a4
Lucas Bergognon <lucas.bergognon@gmail.com>
2017-10-08 11:53:11 +0200

    Add documentation to util function 'fileInUserPath'

--- src/app/models/orm/util.ts
+++ src/app/models/orm/util.ts
@@ -4,7 +4,9 @@ import { remote } from 'electron';
 const app = remote.app;
 
 /**
- *
+ * Gets a valid path in user data folder.
+ * @param {...string[]} filePathElements Paths that must be added (variadic arg)
+ * @return {string} A valid path for the host OS, prepended by the userData folder
  */
 export function fileInUserPath(...filePathElements: string[]): string {
   return path.join(...[app.getPath('userData')].concat(filePathElements));
